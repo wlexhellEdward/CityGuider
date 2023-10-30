ed0125b39cd44d7ff12819411610f677d6ed22c5
wlex <lllvalerall@mail.ru>
2023-10-30 11:40:26 +0300

    Configured Dynamic Base Path for Routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,14 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.tsx'
 import { persistor, store } from './store/store.ts'
+import { basePath } from 'utils/basePath.ts'
+
+   
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename={basePath}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
