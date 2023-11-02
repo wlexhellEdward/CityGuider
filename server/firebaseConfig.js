f4f24aeafb1722948e6917b81c7fc54555a14dd3
wlex <lllvalerall@mail.ru>
2023-11-02 17:42:29 +0300

    fix: fixes for netlify

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
+
 function initializeFirebase(admin) {
-    const serviceAccount = require("./keys/firebaseKey.json");
     const config = {
         credential: admin.credential.cert({
-            projectId: serviceAccount.project_id,
-            clientEmail: serviceAccount.client_email,
-            privateKey: serviceAccount.private_key,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Преобразуйте символы \n обратно в переводы строк
         }),
-        databaseURL: serviceAccount.databaseURL
+        databaseURL: process.env.FIREBASE_DATABASE_URL
     }
     admin.initializeApp(config);
 }
