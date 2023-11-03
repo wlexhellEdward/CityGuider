9f8bd8e26884bb3f9919363d1dcf45cf944b5acf
wlex <lllvalerall@mail.ru>
2023-11-03 12:49:27 +0300

    feat: taking out the logic of the makeRoute function in the hook

---
+++ client/src/hooks/useRoute.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import { ILocation } from '@/interfaces/ILocations';
+import { getDirections } from '@/utils/route';
+
+interface Params {
+    origin: ILocation,
+    destination: ILocation
+}
+
+export const useRoute = ({ origin, destination }: Params) => {
+    const [directions, setDirections] = useState<google.maps.DirectionsResult | null>(null);
+    const [distanceTotal, setDistanceTotal] = useState<number>(0);
+    const [placeLocation, setPlaceLocation] = useState<{ lat: number; lng: number }>({ lat: 0, lng: 0 });
+    const [time, setTime] = useState<string>('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const directionRequest = {
+                origin: origin,
+                destination: destination,
+                travelMode: google.maps.TravelMode.WALKING,
+            };
+
+            const result = await getDirections(directionRequest);
+            const distance = result?.routes[0].legs[0].distance?.value || 0;
+            const time = result?.routes[0].legs[0].duration?.text || '';
+
+            setDistanceTotal(distance)
+            setPlaceLocation(destination)
+            setTime(time)
+            setDirections(result)
+        };
+
+        fetchData();
+    }, [destination, origin]);
+
+    return {
+        directions,
+        distanceTotal,
+        placeLocation,
+        time,
+    };
+};
