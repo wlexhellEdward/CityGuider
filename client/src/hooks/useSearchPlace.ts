b00198f79d665b58a5a814f156c6eddb81dc65ff
wlex <lllvalerall@mail.ru>
2023-11-09 11:55:41 +0300

    fix: correction of the function for finding places through promisification

--- client/src/hooks/useSearchPlace.ts
+++ client/src/hooks/useSearchPlace.ts
@@ -18,32 +18,41 @@ export const useSearchPlace = ({ map, center, inputValue }: Params) => {
     const [results, setResults] = useState<google.maps.places.PlaceResult[]>([])
     const selectedItems = useTypeSelector(state => state.searchSlice.selectedItems)
     useEffect(() => {
-        setResults([])
+        setResults([]);
         if (inputValue.length > 0) {
             if (!map) {
-                return
+                return;
             }
-            selectedItems.forEach(async selectedItem => {
+            const promises: Promise<google.maps.places.PlaceResult[]>[] = [];
+            selectedItems.forEach(selectedItem => {
                 const request = {
                     location: center,
                     radius: Number(inputValue) * 520,
-                    keyword: selectedItem
-                }
-                const placesServices = new google.maps.places.PlacesService(map)
-                await placesServices.nearbySearch(request, (results, status, pagination) => {
-                    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-                        results.forEach(result => {
-                            result.icon = Places.find(place => place.type === selectedItem)?.img
-                        })
-                        setResults(prevResults => [...prevResults, ...results]);
-                        if (pagination?.hasNextPage) {
-                            pagination.nextPage()
-                        }
-                    }
-                })
-            })
+                    keyword: selectedItem,
+                };
+                const placesServices = new google.maps.places.PlacesService(map);
+                promises.push(
+                    new Promise((resolve) => {
+                        placesServices.nearbySearch(request, (results, status) => {
+                            if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+                                results.forEach((result) => {
+                                    result.icon = Places.find(
+                                        (place) => place.type === selectedItem
+                                    )?.img;
+                                });
+                                resolve(results);
+                            } else {
+                                resolve([]);
+                            }
+                        });
+                    })
+                );
+            });
+            Promise.all(promises).then((results) => {
+                const flattenedResults = results.reduce((acc, curr) => [...acc, ...curr], []);
+                setResults(flattenedResults);
+            });
         }
-    }, [inputValue, selectedItems])
+    }, [inputValue, selectedItems]);
     return results
-
 }
