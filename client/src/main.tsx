b2dbd3d3ad1fa43b29627d5d27fc93ca2e3ff5ca
wlex <lllvalerall@mail.ru>
2023-11-11 12:14:59 +0300

    fix: fix imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
