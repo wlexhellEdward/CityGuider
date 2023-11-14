d62797690e217c95dc3daf2c553496377ba534ca
wlex <lllvalerall@mail.ru>
2023-11-14 11:23:22 +0300

    fix:fixes for hosting

--- server/server.js
+++ server/server.js
@@ -11,8 +11,18 @@ const userRoutes = require("./routes/userRoutes");
 initializeFirebase(admin);
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: ["http://localhost:5173", "https://cityguider-api-prod.up.railway.app/"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use("/api/users", userRoutes);
+app.use('/', (req, res) => {
+    res.send('server part is live')
+})
 
 
 app.listen(port, () => {
