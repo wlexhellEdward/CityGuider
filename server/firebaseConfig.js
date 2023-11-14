9c8f4091d5c333e6e3e9e1df3ae654128ae57faf
wlex <lllvalerall@mail.ru>
2023-11-14 10:56:57 +0300

    fix:fixes for hosting

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -5,9 +5,9 @@ dotenv.config();
 function initializeFirebase(admin) {
     const config = {
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID || keys.project_id,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL || keys.client_email,
-            privateKey: (process.env.FIREBASE_PRIVATE_KEY || keys.private_key).replace(/\\n/g, '\n'),
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         }),
         databaseURL: process.env.FIREBASE_DATABASE_URL,
     };
