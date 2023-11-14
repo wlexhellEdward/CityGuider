314fed6e3fdb42e4a3e026912cc52dbf22c01e61
wlex <lllvalerall@mail.ru>
2023-11-14 10:48:53 +0300

    fix: fix .env of back-end for railway

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,14 +1,17 @@
-const keys = require('./keys/firebaseKey.json')
+const dotenv = require('dotenv');
+const keys = require('./keys/firebaseKey.json');
+
+dotenv.config();
 
 function initializeFirebase(admin) {
     const config = {
         credential: admin.credential.cert({
-            projectId: keys.project_id,
-            clientEmail: keys.client_email,
-            privateKey: keys.private_key.replace(/\\n/g, '\n'), 
+            projectId: process.env.FIREBASE_PROJECT_ID || keys.project_id,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL || keys.client_email,
+            privateKey: (process.env.FIREBASE_PRIVATE_KEY || keys.private_key).replace(/\\n/g, '\n'),
         }),
-        databaseURL: process.env.FIREBASE_DATABASE_URL
-    }
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+    };
     admin.initializeApp(config);
 }
 
